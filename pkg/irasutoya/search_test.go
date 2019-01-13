f14d03fa6bd60ace4d4049f15afae025a1e6d29c
Ryota Kayanuma <picosushi12+git@gmail.com>
2019-01-13 21:17:09 +0900

    Add tests

--- pkg/irasutoya/search_test.go
+++ pkg/irasutoya/search_test.go
@@ -9,13 +9,22 @@ func TestSearch(t *testing.T) {
 	if err != nil {
 		t.Fatalf("failed test %#v", err)
 	}
+
+	ikura := SearchResult{}
 	ok := false
 	for _, sr := range sres {
 		if sr.Description == "イクラのお寿司のイラスト" {
 			ok = true
+			ikura = sr
 		}
 	}
 	if !ok {
 		t.Fatalf("Couldn't find イクラのお寿司のイラスト.")
+	}
+	if ikura.EntryURL != "https://www.irasutoya.com/2013/04/blog-post_8611.html" {
+		t.Fatalf("EntryURL is odd. changed?")
+	}
+	if ikura.ThumbnailURL == "" {
+		t.Fatalf("ThumbnailURL not set.")
 	}
 }
